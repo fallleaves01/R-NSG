d699fba9de88f99ab61603a6d9de1c08ec965502
fallleaves01 <adoubiq@qq.com>
2025-07-31 18:59:49 +0800

    feat: init graph

---
+++ include/Graph/Concepts.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <PCH.hpp>
+
+namespace TDFANN {
+
+namespace Graph {
+
+// 检查是否有get_neighbours方法
+template <typename T>
+concept GraphLike =
+    requires(const T& obj) { obj.get_neighbours(); };
+
+}
+
+}  // namespace TDFANN
---
+++ include/Graph/GraphIndex.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <PCH.hpp>
+
+namespace TDFANN {
+
+namespace Graph {
+
+template <typename T>
+class GraphIndex {
+};
+
+}
+
+}  // namespace TDFANN
