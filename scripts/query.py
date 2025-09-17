f94fe16d900c0730d3a0b0404593aaaa7a702f27
fallleaves01 <adoubiq@qq.com>
2025-09-17 21:46:26 +0800

    feat: 添加脚本和设置

--- scripts/query.py
+++ scripts/query.py
@@ -1,6 +1,6 @@
 from subprocess import run
 
-# ../build/linux/x86_64/release/TDFANN query -v ../dataset/sift/sift_base.fvecs -i ../test_data/tdfg_100.graph -n 10 -b 300 -q ../dataset/sift/sift_query.fvecs -r ../test_data/sift1m_result_10.idx -a ../test_data/sift1m_ans_10.idx
+# ../build/linux/x86_64/release/TDFANN query -v ../dataset/sift/sift_base.fvecs -i ../test_data/tdfg_routing_300.graph -n 10 -b 300 -q ../dataset/sift/sift_query.fvecs -r ../test_data/sift1m_result_10.idx -a ../test_data/sift1m_ans_10.idx
 
 for i in [50, 100, 150, 200, 250, 300]:
     for j in [20, 50, 75, 100, 150, 200, 250, 300]:
--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,6 @@
 from subprocess import run
 
+#../build/linux/x86_64/release/TDFANN --verbose build -v ../dataset/sift/sift_base.fvecs -k ../test_data/knng_300.graph -i ../test_data/tdfg_300.graph
 # for i in [50, 100, 150, 200, 250, 300]:
 #     run(["../build/linux/x86_64/release/knng_builder", "--verbose", "-v", "../dataset/sift/sift_base.fvecs", "-k", str(i), "-g", f"../test_data/knng_{i}.graph"])
 
