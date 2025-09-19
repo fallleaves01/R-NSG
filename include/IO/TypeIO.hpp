cdf46fe5c39ddbd71cffcf797bd024e9ca410080
fallleaves01 <adoubiq@qq.com>
2025-09-19 22:53:41 +0800

    ref: 修改include格式

--- include/IO/TypeIO.hpp
+++ include/IO/TypeIO.hpp
@@ -1,6 +1,8 @@
 #pragma once
-#include <IO/Concepts.hpp>
 #include <PCH.hpp>
+
+#include <IO/Concepts.hpp>
+#include <nlohmann/json.hpp>
 
 namespace TDFANN {
 
@@ -79,6 +81,59 @@ std::optional<T> load(std::istream& fin) {
     return obj;
 }
 
+inline std::pair<unsigned, unsigned> get_fvecs_size(std::ifstream& fin) {
+    unsigned dimension = 0;
+    fin.read((char*)&dimension, sizeof(unsigned));
+    fin.seekg(0, std::ios::end);
+    std::ios::pos_type ss = fin.tellg();
+    size_t file_size = ss;
+    unsigned n = file_size / (dimension + 1) / sizeof(float);
+    fin.seekg(0, std::ios::beg);
+    spdlog::info("Vector dimension: {}, size: {}", dimension, n);
+    return {n, dimension};
+}
+
+template <typename T>
+void read_fvecs(std::ifstream& fin, unsigned dimension, T* data) {
+    unsigned tmp, idx = 0;
+    while (fin.read(reinterpret_cast<char*>(&tmp), sizeof(unsigned))) {
+        if (!fin.read(reinterpret_cast<char*>(data + idx * dimension),
+                      dimension * sizeof(T))) {
+            spdlog::error("Failed to read vector data from file");
+            throw std::runtime_error("Failed to read vector data from file");
+        }
+        idx++;
+        if (tmp != dimension) {
+            spdlog::error("Inconsistent vector dimensions");
+            throw std::runtime_error("Inconsistent vector dimensions in file");
+        }
+    }
+}
+
+inline std::vector<size_t> load_json_to_vec(const std::string& filename) {
+    std::ifstream fin(filename);
+    if (!fin.good()) {
+        spdlog::error("Failed to open json file: {}", filename);
+        throw std::runtime_error("Failed to open json file");
+    }
+    nlohmann::json j;
+    fin >> j;
+    if (!j.is_array()) {
+        spdlog::error("JSON file does not contain an array: {}", filename);
+        throw std::runtime_error("JSON file does not contain an array");
+    }
+    std::vector<size_t> result;
+    result.reserve(j.size());
+    for (const auto& item : j) {
+        if (!item.is_number_unsigned()) {
+            spdlog::error("JSON array contains non-unsigned integer values");
+            throw std::runtime_error(
+                "JSON array contains non-unsigned integer values");
+        }
+        result.push_back(item.get<size_t>());
+    }
+    return result;
+}
 }  // namespace IO
 
 }  // namespace TDFANN
